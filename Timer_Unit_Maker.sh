328801a9fc80e57acfdd3e3a4640f3766beb8845
Ethan-Spikes <83477924+Ethan-Spikes@users.noreply.github.com>
2024-08-11 18:33:32 -0400

    Update Timer_Unit_Maker.sh
    Completely made and tested. Confirmed good, so long as input is proper.
    
    Supports creation of a basic timer unit for systemd.
    
    allows for direct editing of the timer file
    
    requires root permissions to run.

--- Timer_Unit_Maker.sh
+++ Timer_Unit_Maker.sh
@@ -1,78 +1,109 @@
-#timer units schedule jobs
-  #jobs can trigger at a certain point in time or when a certain event occurs
+#!/bin/bash
 
-#.timer files control .service files, and both files are required to make a timer unit
+#the previous timer unit maker would fill in variables and make the documents for you.
 
-#timer units are created as systemd services, and therefore are controlled with the 'systemctl' cmd and its logs are accessed with the 'journalctl' cmd
+#I want this one to make a document and format it for me, then tell me how to fill out the document
 
-#so how do we create a timer?
+#I want it to fill out the .timer file and the .service file
 
-#version: 1.0
+#I want it to check the file after I edit it
+
+#I want there to be seamless transitions between executing the script, naming the file, editing the file, checking the file, and moving on to the next file or timer unit
+
+#I want it to check for root first of course
+
+#version: 2.0
+
+path2timers=/etc/systemd/system/
+name=''
 
 
-Set_Variables() {
 
-echo "What do you want the name of your timer to be?"
-read name
-echo "what do you want its description to be?"
-read description
-echo "what do you want the service to execute?"
-read execute
-echo "after system boot, when do you want the timer to run (example: 5min)"
-read B_Time
-echo "while running, how often do you want the timer to run (example: 24h)"
-read A_Time
+
+Root_Check() { #good
+
+	user=$(whoami)
+	if [ $user != root ]; then
+		echo 'Permission denied.'
+		echo ' '
+		exit
+	fi
 
 }
 
-Create_Timer_Unit() {
+Name_File() {
 
-  path2timers=/etc/systemd/system/
-  path2execute=/usr/local/bin/
-  service=$path2timers$name.service
-  timer=$path2timers$name.timer
-  verify=$path2timers$name.*
-  execute=$path2execute$name.sh
-  
-  echo "[Unit]
-  Description=\"$description\"
-  
-  [Service]
-  ExecStart=$execute"  > $service #to make service file
+	echo "What do you want the name of your timer to be?"
+	read name
+	service_file=$path2timers$name.service
+	timer_file=$path2timers$name.timer
+	verify=$path2timers$name.*
 
+}
 
-  
-  
-  echo "[Unit]
-  Description=\"$description\"
-  
-  [Timer]
-  OnBootSec=$B_Time
-  OnUnitActiveSec=$A_Time
-  OnCalendar=Mon..Fri *-*-* 10:00:*
-  Unit=$service
-  
-  [Install]
-  WantedBy=multi-user.target" > $timer #to make timer file
+Generate_Service() {
 
-  systemd-analyze verify $verify #to verify timer unit formatted correctly
+	echo "[Unit]
+Description=User made timer
 
-  sudo systemctl start $name.timer #to activate timer for current session only
+[Service]
+ExecStart="" #full path to executed script"  > $service_file #to make service file
 
-  sudo systemctl enable $name.timer #to enable timer to activate on boot
+}
+Edit_Service() {
 
+	sudo vim $service_file
+
+}
+
+Generate_Timer() {
+
+	echo "[Unit]
+Description=User made timer
+
+[Timer]
+OnBootSec=1min
+OnUnitActiveSec=1min
+Unit=$name.service
+
+[Install]
+WantedBy=multi-user.target" > $timer_file #to make timer file
+
+}
+
+Edit_Timer() {
+
+	sudo vim $timer_file
+
+}
+Check_Timer() {
+
+	sudo systemd-analyze verify $verify #to verify timer unit formatted correctly
+
+}
+
+Enable_Timer_Unit() {
+
+	sudo systemctl start $name.timer #to activate timer for current session only
+
+	sudo systemctl enable $name.timer #to enable timer to activate on boot
 
 }
 
 Main() {
 
-  Root_Check
-  Set_Variables
-  Create_Timer_Unit
-
-  echo "Timer Unit Maker Complete"
-  exit
-  
+	Root_Check
+	Name_File
+	Generate_Service
+	Edit_Service
+	Generate_Timer
+	Edit_Timer
+	Check_Timer
+	Enable_Timer_Unit
+	
+	echo "Script complete"
+	exit
+	
 }
 
 Main
