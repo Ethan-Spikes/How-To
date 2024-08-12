483030ce7a1397608a3c7c468e18d86301c83668
Ethan-Spikes <83477924+Ethan-Spikes@users.noreply.github.com>
2024-08-11 22:49:09 -0400

    Update Auto_Updates.sh

--- Auto_Updates.sh
+++ Auto_Updates.sh
@@ -1,6 +1,6 @@
 Auto_System_Updates() {  #function good
 
-		echo "0 1 * * * echo 'testing' >> /home/kali/Desktop/Projects/log.log" | sudo crontab -
+		echo "1 * * * * echo 'testing' >> /home/kali/Desktop/Projects/log.log" | sudo crontab -
 
 
 
