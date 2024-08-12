ad9c3155d3bb0901a6ab7b0f7dec4c78bb9321e2
Ethan-Spikes <83477924+Ethan-Spikes@users.noreply.github.com>
2024-08-11 22:48:53 -0400

    Update Auto_Updates.sh

--- Auto_Updates.sh
+++ Auto_Updates.sh
@@ -1,9 +1,6 @@
 Auto_System_Updates() {  #function good
 
-		echo "0 1 * * * apt -y update 
-1 1 * * * apt -y upgrade 
-2 1 * * * apt -y autoremove 
-3 1 * * * apt -y autoclean " | sudo crontab -
+		echo "0 1 * * * echo 'testing' >> /home/kali/Desktop/Projects/log.log" | sudo crontab -
 
 
 
